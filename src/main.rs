3237f039a7677514249bab496dba66e118c485b4
lfpraca <luisfelippe@protonmail.com>
2023-05-26 15:36:12 -0300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use actix_web::{web::Data, App, HttpServer};
+use std::io::Result;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::fs;
+use toml;
+use serde::Deserialize;
+
+mod services;
+use services::update_ip;
+
+pub struct AppState {
+    db: Pool<Postgres>,
+    dest_port: u16,
+    final_reject: bool,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    database: DatabaseConfig,
+    server: ServerConfig,
+}
+
+#[derive(Deserialize)]
+struct DatabaseConfig {
+    url: String,
+}
+
+#[derive(Deserialize)]
+struct ServerConfig {
+    port: u16,
+    dest_port: u16,
+    final_reject: bool,
+}
+
+fn read_config() -> Config {
+    let config_content = fs::read_to_string("/etc/auth-iptables/config.toml").expect("Error reading /etc/auth-iptables/config.toml");
+    let config: Config = toml::from_str(&config_content).expect("Error parsing /etc/auth-iptables/config.toml");
+    config
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let config: Config = read_config();
+    if config.server.dest_port == 22 {
+        panic!("For safety, dest_port may not be set to 22");
+    }
+
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&config.database.url)
+    .await
+    .expect("Error creating connection pool to the PostgreSQL server");
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(Data::new(AppState {db: pool.clone(), dest_port: config.server.dest_port, final_reject: config.server.final_reject}))
+        .service(update_ip)
+    })
+    .bind(("127.0.0.1", config.server.port))?
+    .run()
+    .await
+}
---
+++ src/services.rs
@@ -0,0 +1,131 @@
+use actix_web::{get, web::{self, Data}, HttpResponse, HttpRequest, Responder};
+use sqlx::{self, FromRow};
+use crate::AppState;
+use std::process::{Command, Stdio};
+
+#[derive(FromRow)]
+struct UserIp {
+    ip_addr: String,
+}
+
+#[get("/{key}")]
+async fn update_ip(state: Data<AppState>, path: web::Path<String>, req: HttpRequest) -> impl Responder {
+    let key = path.into_inner();
+
+    let new_ip = match req.peer_addr()
+    .filter(|addr| addr.is_ipv4())
+    .map(|addr| addr.ip().to_string()) {
+        Some(ip) => ip,
+        None => return HttpResponse::BadRequest().body("Client IP must be IPv4, contact the administrator if this needs to be changed\n"),
+    };
+
+    let old_ip: String = match sqlx::query_as::<_, UserIp>("SELECT ip_addr FROM auth WHERE \"key\" = $1")
+    .bind(&key)
+    .fetch_one(&state.db)
+    .await {
+        Ok(entry) => entry.ip_addr,
+        Err(_) => return HttpResponse::Unauthorized().body("Unauthorized key\n"),
+    };
+
+    Command::new("sudo")
+    .args(&[
+        "iptables", "-D", "INPUT",
+        "-p", "tcp", "-m", "state",
+        "--state", "NEW", "-m", "tcp",
+        "--dport", &state.dest_port.to_string(),
+        "-s", &old_ip, "-j", "ACCEPT"
+    ])
+    .stdout(Stdio::null())
+    .stderr(Stdio::null())
+    .spawn()
+    .expect("Failed to execute iptables with sudo");
+
+    Command::new("sudo")
+    .args(&[
+        "iptables", "-D", "INPUT",
+        "-p", "udp", "-m", "state",
+        "--state", "NEW", "-m", "udp",
+        "--dport", &state.dest_port.to_string(),
+        "-s", &old_ip, "-j", "ACCEPT"
+    ])
+    .stdout(Stdio::null())
+    .stderr(Stdio::null())
+    .spawn()
+    .expect("Failed to execute iptables with sudo");
+
+    {
+        let tcp_output = Command::new("sudo")
+        .args(&[
+            "iptables", "-A", "INPUT",
+            "-p", "tcp", "-m", "state",
+            "--state", "NEW", "-m", "tcp",
+            "--dport", &state.dest_port.to_string(),
+              "-s", &new_ip, "-j", "ACCEPT"
+        ])
+        .output()
+        .expect("Failed to execute iptables with sudo");
+        if !tcp_output.status.success() {
+            return HttpResponse::InternalServerError().body("Error creating new tcp input rule\n");
+        }
+    }
+
+    {
+        let udp_output = Command::new("sudo")
+        .args(&[
+            "iptables", "-A", "INPUT",
+            "-p", "udp", "-m", "state",
+            "--state", "NEW", "-m", "udp",
+            "--dport", &state.dest_port.to_string(),
+              "-s", &new_ip, "-j", "ACCEPT"
+        ])
+        .output()
+        .expect("Failed to execute iptables with sudo");
+        if !udp_output.status.success() {
+            return HttpResponse::InternalServerError().body("Error creating new udp input rule\n");
+        }
+    }
+
+    let mut update_error = false;
+    if let Err(_) = sqlx::query("UPDATE auth SET ip_addr = $1 WHERE \"key\" = $2")
+    .bind(&new_ip)
+    .bind(&key)
+    .execute(&state.db)
+    .await {
+        update_error = true;
+    }
+
+    if state.final_reject {
+        {
+            let delete_output = Command::new("sudo")
+            .args(&[
+                "iptables", "-D", "INPUT",
+                "-j", "REJECT",
+                "--reject-with", "icmp-host-prohibited"
+            ])
+            .output()
+            .expect("Failed to execute iptables with sudo");
+            if !delete_output.status.success() {
+                return HttpResponse::InternalServerError().body("Error deleting final reject input rule\n");
+            }
+        }
+        {
+            let create_output = Command::new("sudo")
+            .args(&[
+                "iptables", "-A", "INPUT",
+                "-j", "REJECT",
+                "--reject-with", "icmp-host-prohibited"
+            ])
+            .output()
+            .expect("Failed to execute iptables with sudo");
+            if !create_output.status.success() {
+                return HttpResponse::InternalServerError().body("Error creating final reject input rule\n");
+            }
+        }
+    }
+
+    if update_error {
+        return HttpResponse::InternalServerError().body("Error editing entry to new ip\n");
+    }
+
+    HttpResponse::Ok().body(format!("{}\n", new_ip))
+}
